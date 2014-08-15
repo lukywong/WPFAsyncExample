2b710aac6af82561117356c5b443e4c9dc723b1a
Luky Wong <lukywong@gmail.com>
2014-08-15 23:09:09 +0800

    async practice

---
+++ AsyncExampleWPF/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AsyncExampleWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultsTextBox.Clear();
+            await SumPageSizesAsync();
+            resultsTextBox.Text += "\r\nControl returned to startButton_Click.";
+        }
+
+        private async Task SumPageSizesAsync()
+        {
+            HttpClient client =
+                new HttpClient() { MaxResponseContentBufferSize = 1000000 };
+            // Make a list of web addresses.
+            List<string> urlList = SetUpURLList();
+
+            var total = 0;
+            foreach (var url in urlList)
+            {
+                // GetURLContents returns the contents of url as a byte array. 
+                byte[] urlContents = await client.GetByteArrayAsync(url);
+
+                DisplayResults(url, urlContents);
+
+                // Update the total.
+                total += urlContents.Length;
+            }   
+
+            // Display the total count for all of the web addresses.
+            resultsTextBox.Text +=
+                string.Format("\r\n\r\nTotal bytes returned:  {0}\r\n", total);
+        }
+
+        private void DisplayResults(string url, byte[] urlContents)
+        {
+            // Display the length of each website. The string format  
+            // is designed to be used with a monospaced font, such as 
+            // Lucida Console or Global Monospace. 
+            var bytes = urlContents.Length;
+            // Strip off the "http://".
+            var displayURL = url.Replace("http://", "");
+            resultsTextBox.Text += string.Format("\n{0,-58} {1,8}", displayURL, bytes);
+        }
+
+        private async Task<byte[]> GetURLContentsAsync(string url)
+        {
+            // The downloaded resource ends up in the variable named content. 
+            var content = new MemoryStream();
+
+            // Initialize an HttpWebRequest for the current URL. 
+            var webReq = (HttpWebRequest)WebRequest.Create(url);
+
+            // Send the request to the Internet resource and wait for 
+            // the response. 
+            // Note: you can't use HttpWebRequest.GetResponse in a Windows Store app. 
+            using (WebResponse response = await webReq.GetResponseAsync())
+            {
+                // Get the data stream that is associated with the specified URL. 
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    // Read the bytes in responseStream and copy them to content.  
+                    await responseStream.CopyToAsync(content);
+                }
+            }
+
+            // Return the result as a byte array. 
+            return content.ToArray();
+        }
+
+        private List<string> SetUpURLList()
+        {
+            var urls = new List<string> 
+            { 
+                "http://msdn.microsoft.com/library/windows/apps/br211380.aspx",
+                "http://msdn.microsoft.com",
+                "http://msdn.microsoft.com/en-us/library/hh290136.aspx",
+                "http://msdn.microsoft.com/en-us/library/ee256749.aspx",
+                "http://msdn.microsoft.com/en-us/library/hh290138.aspx",
+                "http://msdn.microsoft.com/en-us/library/hh290140.aspx",
+                "http://msdn.microsoft.com/en-us/library/dd470362.aspx",
+                "http://msdn.microsoft.com/en-us/library/aa578028.aspx",
+                "http://msdn.microsoft.com/en-us/library/ms404677.aspx",
+                "http://msdn.microsoft.com/en-us/library/ff730837.aspx"
+            };
+            return urls;
+        }
+    }
+}
